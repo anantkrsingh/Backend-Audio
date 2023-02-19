5d9193a5e1e4a2a1579d60c63e9ea3c23fd46c54
anantkrsingh <anantkrplay@gmail.com>
2023-02-19 18:49:29 +0530

    fix

--- src/Controllers/Auth.js
+++ src/Controllers/Auth.js
@@ -4,17 +4,20 @@ const User = require('../Models/user');
 exports.signup = (req,res) =>  {
         User.findOne({email: req.query.name}).exec((error,user)=>{
             if(user) res.json({status:0,message:"User Already Registered"});
-            const {name , email , phone , password } = req.query;
-            const _user = new User({
-                name,
-                email,
-                phone,
-                password,
-                isVerified:false
-            });
-            _user.save((error,data)=>{
-                if(error) res.json({status:0,message:error})
-                if(data) res.json({status:1, message:"User Successfully Registered"})
-            })
+            else{
+                const {name , email , phone , password } = req.query;
+                const _user = new User({
+                    name,
+                    email,
+                    phone,
+                    password,
+                    isVerified:false
+                });
+                _user.save((error,data)=>{
+                    if(error) res.json({status:0,message:error})
+                    if(data) res.json({status:1, message:"User Successfully Registered"})
+                })
+            }
+           
         })
 }
