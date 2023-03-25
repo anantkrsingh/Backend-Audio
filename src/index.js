81c29ce5d1ec2da44235c52ffea3b2cd723f0528
anantkrsingh <anantkrplay@gmail.com>
2023-03-25 12:09:22 +0530

    cors-fixed

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ const roomRoutes = require('./Routes/Generator')
 const { createMeeting } = require('./Controllers/Generator');
 const app = express();
 
+
+app.use(cors);
 app.use(express.json())
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:true}))
 app.use('/api',authRoutes);
 app.use('/api/meeting',roomRoutes )
-app.use(cors);
 
 
 
